f07a510558c34386dc2c8f706514502f147ab31c
PittaShirisha-hub <pshirisha501@gmail.com>
2026-01-09 21:00:45 +0530

    Q2

---
+++ question-2/index.js
@@ -0,0 +1,11 @@
+console.log("Index file running");
+
+// index.js
+
+import { checkPrime } from "./math.js";
+
+console.log(checkPrime(2));   // true
+console.log(checkPrime(7));   // true
+console.log(checkPrime(10));  // false
+console.log(checkPrime(15));  // false
+console.log(checkPrime(17));  // true
